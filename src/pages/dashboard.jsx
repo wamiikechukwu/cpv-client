b3c06e342f7f8b15434e7b824fec3f7a1e26c796
wami ikechukwu <wamiikechukwu@gmail.com>
2024-01-02 16:55:52 +0100

    fix: get local storage

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,11 @@
+import { useLocalStorage } from "../hooks/useLocalStorage"
+
 export default function Dashboard(){
+
+    const {getItem} = useLocalStorage('user')
+
+    const isTokenValid = getItem.token
+
     return(
         <h1>This is the Dashboard</h1>
     )
