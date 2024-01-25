6acc876fa532bf0b3d454aaac3abddbbade0d7f1
wami ikechukwu <wamiikechukwu@gmail.com>
2024-01-25 11:28:58 +0100

    fix: sidebar navlink styled

--- src/components/layouts/dashboard_layout.jsx
+++ src/components/layouts/dashboard_layout.jsx
@@ -16,12 +16,12 @@ const DashboardLayout = () => {
             <div
                 className={sideBarStyles.navlinkContainer}>
                 <SideBar items={[
-                    { label: 'Home' , icon: <FiAirplay className={sideBarStyles.navIcon}/>, link: '/home' },
-                    { label: 'Deploy GPUs', icon: <FiCommand className={sideBarStyles.navIcon}/>, link: '/deploy_gpus' },
-                    { label: 'Servers', icon: <FiFeather className={sideBarStyles.navIcon}/>, link: '/servers' },
-                    { label: 'Account', icon: <FiCpu className={sideBarStyles.navIcon}/>, link: '/account', },
-                    { label: 'Help', icon: <FiLoader className={sideBarStyles.navIcon}/>, link: '/help' },
-                    { label: 'Sign Out', icon: <FiKey className={sideBarStyles.navIcon}/>, link: 'sign-out' }
+                    { label: 'Home', icon: <FiAirplay className={sideBarStyles.navIcon} />, link: '/home' },
+                    { label: 'Deploy GPUs', icon: <FiCommand className={sideBarStyles.navIcon} />, link: '/deploy_gpus' },
+                    { label: 'Servers', icon: <FiFeather className={sideBarStyles.navIcon} />, link: '/servers' },
+                    { label: 'Account', icon: <FiCpu className={sideBarStyles.navIcon} />, link: '/account', },
+                    { label: 'Help', icon: <FiLoader className={sideBarStyles.navIcon} />, link: '/help' },
+                    { label: 'Sign Out', icon: <FiKey className={sideBarStyles.navIcon} />, link: 'sign-out' }
                 ]} />
             </div>
             <div>
--- src/components/libs/styles/styles.tailwind.js
+++ src/components/libs/styles/styles.tailwind.js
@@ -2,7 +2,7 @@ const sideBarStyles = {
 
     sidebar: 'bg-slate-500 inline-block h-full',
     section: 'inline-block h-full p-[1rem]',
-    navlink: 'block',
+    navlink: 'block p-3.5',
     navlinkContainer: 'fixed h-full',
     navIcon: 'inline-block mr-[1em] w-[20px] h-[20px]'
 }
